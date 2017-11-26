8827bd14bb0067cdb239138b1bc7d01985e20cd8
Kaczi <kaczi89@gmal.com>
2017-11-26 08:31:39 +0100

    texture flipY

--- Texture.cs
+++ Texture.cs
@@ -19,6 +19,7 @@ namespace Axon
             {
                 Console.WriteLine("Unable to load texture: " + e.Message);
             }
+                this.Bitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
 
                 this.Id = GL.GenTexture();
                 GL.BindTexture(TextureTarget.Texture2D, this.Id); 
@@ -29,7 +30,6 @@ namespace Axon
                 GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Linear);
 
                 GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, this.Bitmap.Width, this.Bitmap.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, IntPtr.Zero);
-    
                 BitmapData bitmap_data = this.Bitmap.LockBits(new Rectangle(0, 0, this.Bitmap.Width, this.Bitmap.Height),
                 ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
